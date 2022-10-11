79d14ee729e061c121b19a8d7a2e078e55a8134a
MsMemer <115589649+MsMemer@users.noreply.github.com>
2022-10-11 23:47:46 +0300

    Лабораторная работа 0

---
+++ task_1.py
@@ -0,0 +1,8 @@
+Python 3.10.8 (v3.10.8:aaaf517424, Oct 11 2022, 10:14:40) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> print ("hello, World")
+hello, World
+>>> print ("Hello World!")
+Hello World!
